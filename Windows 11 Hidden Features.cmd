bb1eb60ffa2b54d1aab418a9abbaee8a99ffb4c5
Moses Makaka <53109687+mosesmakaka@users.noreply.github.com>
2023-02-27 18:36:56 +0300

    Updates

---
+++ Windows 11 Hidden Features.cmd
@@ -0,0 +1,78 @@
+vivetool /enable /id:41740192
+vivetool /enable /id:41561445
+vivetool /enable /id:41561454
+vivetool /enable /id:36165848
+vivetool /enable /id:40733296 
+vivetool /enable /id:41562961
+vivetool /enable /id:34592303
+vivetool /enable /id:35620393
+vivetool /enable /id:36302090
+vivetool /enable /id:37007953
+vivetool /enable /id:39281392
+vivetool /enable /id:39145991
+vivetool /enable /id:39281392
+vivetool /enable /id:38823293
+vivetool /enable /id:34618575
+vivetool /enable /id:40059556
+vivetool /enable /id:40059556 /variant:3
+vivetool /enable /id:40059556 /variant:4
+vivetool /enable /id:41539325
+vivetool /enable /id:41539325 /variant:2
+vivetool /enable /id:36390579 
+vivetool /enable /id:41670003
+vivetool /enable /id:39405701
+vivetool /enable /id:39731733
+vivetool /enable /id:41075629
+vivetool /enable /id:38113452
+vivetool addconfig 35674037 2
+vivetool addconfig 36226836 2
+vivetool /enable /id:39710659
+vivetool /enable /id:39880030
+vivetool /enable /id:40268500
+vivetool /enable /id:26008830
+vivetool /enable /id:34878152
+vivetool /enable /id:40887771
+vivetool /enable /id:41415839
+vivetool /enable /id:38937525
+vivetool /enable /id:41740192
+vivetool /enable /id:41561445
+vivetool /enable /id:41561454
+vivetool /enable /id:39427030
+vivetool /enable /id:41249924
+vivetool /enable /id:36435151
+vivetool /enable /id:36094217
+vivetool /enable /id:38665217
+vivetool /enable /id:41040327
+vivetool /enable /id:34924843
+vivetool /enable /id:40430431
+vivetool /enable /id:40618079
+vivetool /enable /id:42106010
+vivetool /enable /id:38613007
+vivetool /enable /id:41040327
+vivetool /enable /id:42295138 
+vivetool /enable /id:39880030
+vivetool /enable /id:40268500 /variant:0
+vivetool /enable /id:40522394
+vivetool /enable /id:41744267
+vivetool /enable /id:41861575 /variant:0
+vivetool /enable /id:42550315
+vivetool /enable /id:39661369
+vivetool /enable /id:42623125
+vivetool /enable /id:41058795
+vivetool /enable /id:41539325
+vivetool /enable /id:35262205
+vivetool /enable /id:41355275
+vivetool /enable /id:42533185
+vivetool /enable /id:42133512
+vivetool /enable /id:42739793
+vivetool /enable /id:42592269
+vivetool /enable /id:40851068 /variant:3
+
+
+
+
+
+
+
+
+
