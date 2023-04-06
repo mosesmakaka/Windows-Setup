1cbb92bb74725c645f3937e0caf7afd7f7c86b54
mosesmakaka <moseskax@gmail.com>
2023-04-06 12:17:05 +0300

    Tinker UI

--- main.py
+++ main.py
@@ -3,23 +3,23 @@ import os
 
 # Create the main window
 window = tk.Tk()
-window.title("Batch File UI")
+window.title("Windows 11 Hidden Features")
 
 # Create a label
-label = tk.Label(window, text="Enter the name of the file you want to create:")
+label = tk.Label(window, text="Click enable to install hidden features")
 label.pack()
 
 # Create an entry box
 entry = tk.Entry(window)
-entry.pack()
+#entry.pack()
 
 # Function to run the batch file
 def run_batch_file():
-    filename = entry.get() + ".txt"
-    os.system("Windows 11 Hidden Features.bat " + filename)
+    filename = entry.get() + ".bat"
+    os.system("hf" + filename)
 
 # Create a button to run the batch file
-button = tk.Button(window, text="Create File", command=run_batch_file)
+button = tk.Button(window, text="Enable", command=run_batch_file)
 button.pack()
 
 # Run the main event loop
